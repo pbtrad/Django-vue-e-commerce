6774cbc17216112e312b9b336c3ea56d68cab757
Paul Browne <paulbrowne2@gmail.com>
2021-06-20 21:34:50 +0100

    Set up view for frontpage.html and corresponding urls, in apps.py changed .core to apps.core as is required with most recent version of django

--- saulgadgets/saulgadgets/urls.py
+++ saulgadgets/saulgadgets/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from os import name
 from django.contrib import admin
 from django.urls import path
 
+from apps.core.views import frontpage
+
 urlpatterns = [
+    path('', frontpage, name='frontpage'),
     path('admin/', admin.site.urls),
 ]
